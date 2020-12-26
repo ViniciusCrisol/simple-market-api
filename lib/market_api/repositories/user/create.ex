229e14967ca9a1e7f6de3bc8697bd1e62516ea17
Vinícius <vinicius.crisol@hotmail.com>
2020-12-26 14:31:56 -0300

    Fixes - Create user

--- lib/market_api/repositories/user/create.ex
+++ lib/market_api/repositories/user/create.ex
@@ -8,6 +8,12 @@ defmodule MarketApi.Repositories.User.Create do
     |> create_user()
   end
 
-  defp create_user({:ok, struct}), do: Repo.insert(struct)
   defp create_user({:error, changeset}), do: {:error, :bad_request, changeset}
+
+  defp create_user({:ok, %{email: email} = struct}) do
+    case Repo.get_by(User, email: email) do
+      nil -> Repo.insert(struct)
+      _user -> {:error, :unauthorized, "E-mail already in use!"}
+    end
+  end
 end
--- lib/market_api_web/controllers/users_controller.ex
+++ lib/market_api_web/controllers/users_controller.ex
@@ -1,24 +1,16 @@
 defmodule MarketApiWeb.UsersController do
   use MarketApiWeb, :controller
 
-  alias MarketApi.Repo
-  alias MarketApi.Schemas.User
   alias MarketApiWeb.Auth.Guardian
 
   action_fallback MarketApiWeb.FallbackController
 
-  def create(conn, %{"email" => email} = params) do
-    case Repo.get_by(User, email: email) do
-      nil ->
-        with {:ok, user} <- MarketApi.create_user(params),
-             {:ok, token, _claims} <- Guardian.encode_and_sign(user) do
-          conn
-          |> put_status(:created)
-          |> render("create.json", %{user: user, token: token})
-        end
-
-      _user ->
-        {:error, :unauthorized, "E-mail already in use!"}
+  def create(conn, params) do
+    with {:ok, user} <- MarketApi.create_user(params),
+         {:ok, token, _claims} <- Guardian.encode_and_sign(user) do
+      conn
+      |> put_status(:created)
+      |> render("create.json", %{user: user, token: token})
     end
   end
 end
